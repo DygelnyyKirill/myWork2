4a8614bfa3f3f14eed63568e2311e707aaaeccb6
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2020-09-23 22:33:55 +0300

    taskWhileFor(js)

---
+++ my 1 work/js/whileFor.js
@@ -0,0 +1,64 @@
+// let arr = [2, 5, 9, 15, 0, 4];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 3 && arr[i] < 10) {
+//         console.log(arr[i]);
+//     };
+// };
+
+// let arr = [2, 5, 7, 4, 6, 20];
+// let str = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(str += arr[i]);
+//     }
+// };
+
+// let arr = [1, 2, 5, 9, 4, 13, 4, 10];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 4) {
+//         alert('yeas');
+//         break;
+//     };
+// };
+
+// let arr = [10, 20, 30, 50, 235, 3000];
+// for ( let i = 0; i < arr.length; i++) {
+//     let str = arr[i].toString()[0];
+//     if (str === '1' || str ==='2' || str === '5') {
+//         console.log(arr[i]);
+//     };
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, ' '];
+// for (let i = 0; i <arr.length; i++) {
+//     let str = arr[i].toString()[0];
+//     if (str == '1' || str == '2' || str == '3' 
+//     || str == '4' || str == '5' || str == '6'
+//     || str == '7' || str == '8' || str == '9'
+//     || str == '') {
+//         document.write(-arr[i]);
+//     };
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let str = '';
+// for (let i = 0; i <arr.length; i++) { 
+//     str += '-' + arr[i];
+// }
+// document.write(str + '-');
+
+// let arr = ['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'];
+// for (let i = 0; i <= 4; i++) {
+//     document.write(arr[i] + '<br>');
+// }
+// document.write(arr[5].bold() + '<br>' + arr[6].bold());
+
+
+// let arr = ['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'];
+// let day = 0;
+// for (let i = 0; i <= 4; i++) {
+//     day = arr[5];
+// }
+// document.write(day.italics());
+
+	
