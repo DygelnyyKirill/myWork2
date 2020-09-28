a3f9ff441de39bc1e3f79e29b5f15733cee0cea2
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2020-09-28 21:26:14 +0300

    functWithStr.js

---
+++ my 1 work/js/stringFunction.js
@@ -0,0 +1,63 @@
+// let str = 'js';
+// console.log(str.toUpperCase());
+
+// let str = 'JS';
+// console.log(str.toLowerCase());
+
+// let str = 'I love javascript!'
+// console.log(str.length);
+
+// let str = 'I am learning js';
+// console.log(str.substr(5, 8));
+// console.log(str.substr(14, ));
+// console.log(str.substring(5, 13));
+// console.log(str.substring(14, ));
+// console.log(str.slice(5, 13));
+// console.log(str.slice(14, 16));
+
+// let str = 'I am learning js';
+// console.log(str.indexOf('learning'));
+
+
+// let str = 'I am learning';
+// let n = 10;
+// if (str.length > n) {
+//     console.log(result = str.substr(0, n));
+//  } else {
+//     console.log(result = str);
+//  }
+
+// let str = 'I-am-learning-js';
+// console.log(str.replace(/-/g, '!'));
+
+// let str = 'I am learning js';
+// let a = str.split(' ')
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+
+// const myString = 'I am learning js!';
+// const myCharacterSplit = myString.split('');
+// console.log(myCharacterSplit);
+
+// const date = '2025-12-31';
+// let arr = date.split('-');
+// let newStr = arr[2] + '-' + arr[1] + '-' + arr[0];
+// console.log(newStr);
+
+// let arr = ['I', 'am', 'learning', 'js', '!'];
+// let str = arr.join('+');
+// console.log(str);
+
+// let str = 'lerning';
+// console.log(str.substring(0, 1).toUpperCase());
+
+// let string = 'I am  the code';
+
+// console.log(
+// string.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
+// );
+
+//task 14 - 
